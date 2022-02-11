747d5073cd8f01533393b2fe96422cbb8b5ed53c
jpb06 <jp.bois.06@outlook.fr>
2022-02-11 11:22:14 +0100

    adding public folder as static dir

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,20 @@
 const tsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/react",
- webpackFinal: async (config) => {
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+  staticDirs: ['../public'],
+  webpackFinal: async (config) => {
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
       new tsconfigPathsPlugin({
         extensions: config.resolve.extensions,
       }),
-    ];
-   return config;
- },
+    ]
+    return config
+  },
 }
