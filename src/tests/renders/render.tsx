335e2fc578b94c70f741bcf84c2412a290cceae4
jpb06 <jp.bois.06@outlook.fr>
2022-02-28 15:33:58 +0100

    adding theme provider to the default tests render

--- src/tests/renders/render.tsx
+++ src/tests/renders/render.tsx
@@ -2,7 +2,7 @@ import { render as rtlRender, RenderResult } from '@testing-library/react';
 import React from 'react';
 
 import { WithSnackbar } from '@organisms';
-import { EmotionCacheProvider } from '@providers';
+import { AppThemeProvider, EmotionCacheProvider } from '@providers';
 
 import { ReactQueryWrapper } from '../wrappers/react-query';
 
@@ -10,9 +10,11 @@ export const render = (component: JSX.Element): RenderResult => {
   const wrapper: React.FC = ({ children }) => {
     return (
       <EmotionCacheProvider>
-        <WithSnackbar>
-          <ReactQueryWrapper>{children}</ReactQueryWrapper>
-        </WithSnackbar>
+        <AppThemeProvider>
+          <WithSnackbar>
+            <ReactQueryWrapper>{children}</ReactQueryWrapper>
+          </WithSnackbar>
+        </AppThemeProvider>
       </EmotionCacheProvider>
     );
   };
