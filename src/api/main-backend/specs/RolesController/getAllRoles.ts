9f8ffc3a64ada70f05ffe27e30ff3a53a5893a9f
jpb06 <jp.bois.06@outlook.fr>
2022-08-03 13:09:24 +0200

    chore: refreshing backend specs

--- src/api/main-backend/specs/RolesController/getAllRoles.ts
+++ src/api/main-backend/specs/RolesController/getAllRoles.ts
@@ -1,11 +1,14 @@
+/* eslint-disable */
+/* tslint:disable */
+
 /** getAllRoles
  * verb: get
  * summary: Gets all roles
  */
 
-import { RolesResultDto, ApiResponseDto } from '../api-types';
+import { RolesResultDto, ApiResponseDto } from './../api-types';
 
-export const path = `${process.env.NEXT_PUBLIC_API_URL}/roles`;
+export const path = `/roles`;
 
 export type GetAllRolesSuccess = RolesResultDto;
 export type GetAllRolesError = ApiResponseDto;
